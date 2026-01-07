147fd9620ecf1cb8bd5e3eb65580c3ac6ee9209c
Umair Ali <legendumair12@gmail.com>
2026-01-07 14:13:11 +0500

    Adding Student Information Class

---
+++ Java /SIBA-Transcript-Generator/Source-Code/info.java
@@ -0,0 +1,5 @@
+public class Info {
+    String name, cms_id, department, s_name[], std_grades[], courseState[], transcript_type;
+    int semester, tot_marks[], c_hours[], prev_Credits;
+    double sub_cgpa[], totalgpa, lastsemgpa;
+}
